ebc06fc9f14262a5c270ca724317c69c106e0361
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-03-14 01:48:19 +0530

    02_functions added

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,18 @@
+function calculatecartprice(...num1){// ... rest operator or spread operator  khudra bikreta ko ek jagh krke dena 
+        return num1
+}
+// calculatecartprice(2)
+// console.log(calculatecartprice(22,33,44))
+
+
+const user ={
+    username: "hitesh",
+    prices: 199
+}
+function handleobject(anyobjecct){
+    console.log(`username is ${anyobjecct.username} and price is ${anyobjecct.price}`);
+}
+handleobject({
+    username: "awdhesh",
+    price: 437
+})
