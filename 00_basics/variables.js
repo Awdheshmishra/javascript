856b6215436a13ae24622f48f9de16dadb82e197
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-06-02 23:09:36 +0530

    updated and added

--- 00_basics/variables.js
+++ 00_basics/variables.js
@@ -1,12 +1,14 @@
+
 var myfirstname_="karan";
-// console.log(myfirstname_);
-
-
-
-
-
-
+// console.log(myfirstname_)
 //we can write or assign a variable in javascript using var keyword
-
 var $mylastname_="mishra";
 console.log(myfirstname_ +" "+ $mylastname_);
+
+var name = "rahul"
+let age = 20
+const PI = 3.14
+age = 21
+console.log(age)
+PI = 31.25
+console.log(PI)
---
+++ object.js
@@ -0,0 +1,43 @@
+const user = {
+    name: "rohit",
+    age: 20,
+    email: "negi@gmail.com",
+    amount: 3400
+}
+//CRUD operations
+/* console.log(typeof user)
+console.log(user.age)
+user.aadhar = 1234
+user.amount = 2000
+console.log(user)
+delete user.email
+console.log(user) */
+
+/* const user2 = user
+user2.age = 30
+console.log(user) */
+
+//Important
+/* console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.entries(user)) */
+
+// for (let num in user)
+//     console.log(num)
+
+// const {name, age} = user
+// console.log(user)
+
+//for off loop
+//ham for of loop ka use karte kyuki Object.keys hame array ke form me value return krke deta hai.  Direct object par for of loop nhi lag skta
+/* const temparr = Object.keys(user)
+console.log(temparr)
+for(let keys of temparr )
+    console.log(keys)
+//for values 
+const temparr2 = Object.values(user)
+console.log(temparr2)
+for(let values of temparr2 )
+    console.log(values)
+
+ */
