3e6be23c89c0bc51bb63862e662da9a5a8ac078c
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-06-20 20:50:28 +0530

    adding hoisting

--- hoisting.js
+++ hoisting.js
@@ -5,3 +5,10 @@
 
 //hoisting is javascript's behaviour of knowing about a variable or function's existence before executing the code. How it treats that knowledge depends on the keyword (function, var, let or const).
 
+
+var a;
+console.log(a)
+a =2;
+
+// undefined jb tumhare ps particular chiz ho par tumhe uski exact value pta nh ho 
+// not defind mtlv existance hi nhi hai
