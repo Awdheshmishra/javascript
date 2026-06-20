b30e203b580f34c29de2760502096186abe105a8
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-06-20 20:49:30 +0530

    adding types.js

---
+++ types.js
@@ -0,0 +1,11 @@
+//primitives and refrence
+
+// primitives -> number , string, null , undefind , boolean
+//refrence = [] () {} jinme bracket ho like array       -- aise koi bhi value jisko copy kanre pr real copy nhi hota blki us main value ka refrence paas ho jata use ham refrence value khte hain, aur jiska copy krne par real copy ho jye use primitive type value kahte hain --
+
+// var a = 12;
+// var b = a;
+var a =[1,2,3,4]
+var b = a;
+b.pop()
+console.log(a)
